ae6ec35633178167986ce89e95a8beeaea7f5850
papa_neto <vspapaneto@gmail.com>
2019-05-03 03:06:26 -0300

    initial

---
+++ script.js
@@ -0,0 +1,36 @@
+
+var playing = false;
+
+function togglePlay() {
+    playing ? weTalk('off') : weTalk('on');
+}
+
+function toggleShow() {
+    playing ? $('.options').addClass('notshow') : $('.options').removeClass('notshow');
+}
+
+$('#btn').click(function () {
+    togglePlay();
+});
+
+$('.pelicula').click(function () {
+    togglePlay();
+});
+
+function weTalk(mode) {
+    console.log('errorThrown');
+    $.ajax({
+        type: "GET",
+        url: 'http://' + $('#ip').val() + '/' + mode,
+        success: function (result) {
+            console.log(result)
+            playing ? $('video').get(0).pause() : $('video').get(0).play();
+            toggleShow();
+            playing = !playing;
+        },
+        error: function (textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+}
+
